766e8c77f0e4d8e4698394eb2e249f4dfe337749
Javier Quintana <rikiquintana7@gmail.com>
2023-10-02 21:44:47 -0500

    firts commit

---
+++ principal/conexion.php
@@ -0,0 +1,21 @@
+<?php
+    class Conexio{
+
+        function ConexionBD(){
+            $host = 'localhost';
+            $dbname = 'Proyecto';
+            $username = 'sa';
+            $pasword = 'Javi-1508';
+            $puerto = '1433';
+
+            try{
+                $com = new PDO("sqlrv:server=$host, $puerto; Database =$dbname, $username, $pasword");
+                echo  'Successful';
+            }
+            catch(PDOException $exp){
+                echo ('Not successfull');
+            }
+        }
+
+    }
+?>
