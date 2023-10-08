3b345ee0272277f52b70b5d67f81b7a2ffdb7ed9
Javier Quintana <rikiquintana7@gmail.com>
2023-10-08 01:39:46 -0500

    git commit

--- Conexion/ConexionSQLServer.java
+++ Conexion/ConexionSQLServer.java
@@ -38,39 +38,3 @@ public class ConexionSQLServer {
         
     }
 }
-
-        /* 
-        // Parámetros de conexión
-        String url = "jdbc:sqlserver://JAVI-LAPTOP:1433; databaseName = Proyecto";
-        String usuario = "as";
-        String contrasena = "Javi-1508";
-
-        try {
-            // Establecer la conexión
-            Connection conn = DriverManager.getConnection(url, usuario, contrasena);
-
-            // Crear una declaración
-            Statement declaracion = conn.createStatement();
-
-            // Realizar una consulta SQL
-            String sql = "SELECT * FROM Registro";
-            ResultSet rs = declaracion.executeQuery(sql);
-
-            // Procesar los resultados
-            while (rs.next()) {
-                int campo1 = rs.getInt("nombre");
-                String campo2 = rs.getString("apellido");
-
-                System.out.println("Nombre: " + campo1 + ", Apellido: " + campo2);
-            }
-
-            // Cerrar la conexión y los recursos
-            rs.close();
-            declaracion.close();
-            conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        */
-    //}
-//}
