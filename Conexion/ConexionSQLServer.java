4721dff71d25a2b93bc37cf35457dcd3bc1396ad
Javier Quintana <rikiquintana7@gmail.com>
2023-10-08 01:38:14 -0500

    git commit

--- Conexion/ConexionSQLServer.java
+++ Conexion/ConexionSQLServer.java
@@ -3,10 +3,43 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.swing.JOptionPane;
 
 public class ConexionSQLServer {
 
+    public Connection HacerConexion(){
+
+             Connection conexion = null;
+
+            String nombre = "as" ;
+            String contrasena = "Javi-1508" ;
+            String bd = "Proyecto";
+            String ip = "localhost";
+            String Puerto = "1433";
+
+            String cadena = "jdbc:sqlserver://"+ip+":"+Puerto+"/"+bd;
+
+            //String cadena = "jdbc:sqlserver://" + ip + ":" + Puerto + "/" + bd;
+
+            try{
+                
+                cadena = "jdbc:sqlserver://localhost:"  + Puerto+":"+"databaseName"+bd;
+                conexion = DriverManager.getConnection(cadena, nombre, contrasena);
+                JOptionPane.showMessageDialog(null, "Se conecto correctamente a la base de datos");
+
+            }catch(Exception e){
+                JOptionPane.showMessageDialog(null, "Error al conectar la base de datos, error: " + e.toString());
+            }
+            
+            return conexion;
+        }
+
     public static void main(String[] args) {
+        
+    }
+}
+
+        /* 
         // Parámetros de conexión
         String url = "jdbc:sqlserver://JAVI-LAPTOP:1433; databaseName = Proyecto";
         String usuario = "as";
@@ -38,5 +71,6 @@ public class ConexionSQLServer {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-}
+        */
+    //}
+//}
