85a58f3ed5fca9cb3d76a83a01e304192c6093ec
Javi Dev <rikiquintana7@gmail.com>
2023-11-28 18:17:37 -0500

    Prueba de teclado y mouse

--- SRC/JS/Prueba.js
+++ SRC/JS/Prueba.js
@@ -1,5 +1,15 @@
-console.log('Hola');
-function comer(){
-    let comida = 'Arroz';
-    let jugo = 'Tomate de arbol';
-}
+let rate = document.getElementById("rateus");
+let boton = document.getElementById("insertar");
+let clean = document.getElementById("Clean");
+rate.addEventListener("click", function(){
+    if(boton => 5){
+        alert("Mayor a 5");
+    }else{
+        alert("Menor a 5")
+    }
+    console.log("HOLAAAA");
+});
+
+clean.addEventListener("click", function(){
+    rate = '';
+});
