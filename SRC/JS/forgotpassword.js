f80114fdd01f391954c341890edb2370260d63cf
Javi Dev <rikiquintana7@gmail.com>
2024-01-10 23:18:50 -0500

    Cambios del JS para reconocer el mail

--- SRC/JS/forgotpassword.js
+++ SRC/JS/forgotpassword.js
@@ -1,14 +1,24 @@
 let seguro = document.getElementById('button');
-seguro.addEventListener('click', function() {
-        Email.send({
-          Host: "smtp.yourmailprovider.com",
-          SecureToken: "BwMIDnOQ76qk0yhF",
-          To: "javierq150804@gmail.com",
-          From: "rikiquintana7@gmail.com",
-          Subject: "Prueba de correp",
-          Body: "Contenido del correo",
-        }).then(
-          message => alert("Correo enviado exitosamente: " 
-          + message)
-        );
+var correo = document.getElementById('input').value;
+
+var review = addEventListener('click', function revisar() {
+  if(this.correo == "jquintana@gmail.com"){
+      this.alert("Hola Javi");
+  }else{
+      this.alert("Correo no registrado");
+  } 
 });
+
+// seguro.addEventListener('click', function() {
+//         Email.send({
+//           Host: "smtp.yourmailprovider.com",
+//           SecureToken: "BwMIDnOQ76qk0yhF",
+//           To: "javierq150804@gmail.com",
+//           From: "rikiquintana7@gmail.com",
+//           Subject: "Prueba de correp",
+//           Body: "Contenido del correo",
+//         }).then(
+//           message => alert("Correo enviado exitosamente: " 
+//           + message)
+//         );
+// });
